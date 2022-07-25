db9e6b06ceb6810dd179ce4ee763d00ff6c6a127
Tom Gregor <tgregor@ivsoftware.com>
2022-07-25 07:17:54 -0600

    Add project files.

---
+++ double_click/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace double_click
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // this.buttonTryDoubleClick = new System.Windows.Forms.Button();
+            this.buttonTryDoubleClick = new double_click.ButtonWithDoubleClick();
+            this.SuspendLayout();
+            // 
+            // buttonTryDoubleClick
+            // 
+            this.buttonTryDoubleClick.Location = new System.Drawing.Point(35, 65);
+            this.buttonTryDoubleClick.Name = "buttonTryDoubleClick";
+            this.buttonTryDoubleClick.Size = new System.Drawing.Size(180, 34);
+            this.buttonTryDoubleClick.TabIndex = 0;
+            this.buttonTryDoubleClick.Text = "Double-Click Me";
+            this.buttonTryDoubleClick.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.buttonTryDoubleClick);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        // private System.Windows.Forms.Button buttonTryDoubleClick; 
+        private double_click.ButtonWithDoubleClick buttonTryDoubleClick;
+    }
+}
+
---
+++ double_click/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace double_click
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonTryDoubleClick.Click += ButtonTryDoubleClick_Click;
+            buttonTryDoubleClick.DoubleClick += ButtonTryDoubleClick_DoubleClick;
+        }
+
+        private async void ButtonTryDoubleClick_Click(object sender, EventArgs e)
+        {
+            _isDoubleClick = false;
+            // Allow some time to get the second click if it's coming.
+            await Task.Delay(100);
+            if(!_isDoubleClick)
+            {
+                MessageBox.Show("Clicked");
+            }
+        }
+
+        bool _isDoubleClick = false;
+        private void ButtonTryDoubleClick_DoubleClick(object sender, EventArgs e)
+        {
+            _isDoubleClick = true;
+            BeginInvoke((MethodInvoker)delegate 
+            {
+                MessageBox.Show("Double Clicked");
+            }); 
+        }
+    }
+    class ButtonWithDoubleClick : Button
+    {
+        const int WM_LBUTTONDBLCLK = 0x203;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WM_LBUTTONDBLCLK:
+                    OnDoubleClick(EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
